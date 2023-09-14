61deef3aed1357e903c85614817889c2a48dbbee
Yarin Cohen <yrcohen8@gmail.com>
2023-09-14 21:49:48 +0300

    Webmap v1

---
+++ map1.py
@@ -0,0 +1,32 @@
+import folium 
+import pandas
+
+data = pandas.read_csv("D:\\test\Volcanoes_USA.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevaion):
+    if elevaion < 1000:
+        return 'green'
+    elif 1000<= elevaion < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="OpenStreetMap")
+
+fgv = folium.FeatureGroup(name="Volcanoes")
+
+for lt, ln, el in zip(lat, lon, elev):
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, fill_opacity=0.7, fill_color=color_producer(el), popup=str(el)+"m", color= 'grey'))
+
+fgp = folium.FeatureGroup(name="Population")
+fgp.add_child(folium.GeoJson(data=open('D:\\test\world.json', 'r' , encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
+map.save("Map1.html")
